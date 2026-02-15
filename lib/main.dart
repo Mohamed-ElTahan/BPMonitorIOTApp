b7e95bb6cfb27666f34aa4b96f60975957f88b9a
Mohamed ElTahan <mosherifeltahan@gmail.com>
2026-02-15 14:44:42 +0200

    Initial project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BPMoniterIotApp());
+}
+
+class BPMoniterIotApp extends StatelessWidget {
+  const BPMoniterIotApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
