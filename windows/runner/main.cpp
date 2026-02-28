417b518497c9bba926ae14933ba65673b07d1fa9
Mohamed ElTahan <mosherifeltahan@gmail.com>
2026-02-28 15:07:59 +0200

    chore: rebrand web and windows app to SBPM and update config

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"bp_monitor_iot", origin, size)) {
+  if (!window.Create(L"SBPM", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
