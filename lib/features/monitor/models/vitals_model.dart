2ce5eab45c1eaa6177599c79260778770157cf38
Mohamed ElTahan <mosherifeltahan@gmail.com>
2026-03-05 16:27:03 +0200

    [Model] Refactor VitalsModel to support List<double> for livePressure

--- lib/features/monitor/models/vitals_model.dart
+++ lib/features/monitor/models/vitals_model.dart
@@ -4,8 +4,9 @@ class VitalsModel extends Equatable {
   final List<double> ecg;
   final int spo2;
   final int heartRate;
-  final int systolicBP;
-  final int diastolicBP;
+  final double systolicBP;
+  final double diastolicBP;
+  final List<double> livePressure;
   final DateTime timestamp;
 
   const VitalsModel({
@@ -14,20 +15,30 @@ class VitalsModel extends Equatable {
     required this.heartRate,
     required this.systolicBP,
     required this.diastolicBP,
+    required this.livePressure,
     required this.timestamp,
   });
 
   factory VitalsModel.fromJson(Map<String, dynamic> json) {
+    List<double> ecgPoints = [];
+    final ecgData = json['ecg'];
+    if (ecgData is List) {
+      ecgPoints = ecgData.map((e) => (e as num).toDouble()).toList();
+    } else if (ecgData is num) {
+      ecgPoints = [ecgData.toDouble()];
+    }
+
     return VitalsModel(
-      ecg:
-          (json['ecg'] as List<dynamic>?)
-              ?.map((e) => (e as num).toDouble())
-              .toList() ??
-          [],
+      ecg: ecgPoints,
       spo2: (json['spo2'] as num?)?.toInt() ?? 0,
       heartRate: (json['hr'] as num?)?.toInt() ?? 0,
-      systolicBP: (json['sys'] as num?)?.toInt() ?? 0,
-      diastolicBP: (json['dia'] as num?)?.toInt() ?? 0,
+      systolicBP: (json['sys'] as num?)?.toDouble() ?? 0.0,
+      diastolicBP: (json['dia'] as num?)?.toDouble() ?? 0.0,
+      livePressure:
+          (json['live_pressure'] as List<num>?)
+              ?.map((e) => e.toDouble())
+              .toList() ??
+          [],
       timestamp: json['timestamp'] != null
           ? DateTime.parse(json['timestamp'].toString())
           : DateTime.now(),
@@ -41,6 +52,7 @@ class VitalsModel extends Equatable {
       'hr': heartRate,
       'sys': systolicBP,
       'dia': diastolicBP,
+      'live_pressure': livePressure,
       'timestamp': timestamp.toIso8601String(),
     };
   }
@@ -51,9 +63,30 @@ class VitalsModel extends Equatable {
       ecg: const [],
       spo2: 0,
       heartRate: 0,
-      systolicBP: 0,
-      diastolicBP: 0,
+      systolicBP: 0.0,
+      diastolicBP: 0.0,
+      livePressure: const [],
       timestamp: DateTime.now(),
+    );
+  }
+
+  VitalsModel copyWith({
+    List<double>? ecg,
+    int? spo2,
+    int? heartRate,
+    double? systolicBP,
+    double? diastolicBP,
+    List<double>? livePressure,
+    DateTime? timestamp,
+  }) {
+    return VitalsModel(
+      ecg: ecg ?? this.ecg,
+      spo2: spo2 ?? this.spo2,
+      heartRate: heartRate ?? this.heartRate,
+      systolicBP: systolicBP ?? this.systolicBP,
+      diastolicBP: diastolicBP ?? this.diastolicBP,
+      livePressure: livePressure ?? this.livePressure,
+      timestamp: timestamp ?? this.timestamp,
     );
   }
 
@@ -64,6 +97,7 @@ class VitalsModel extends Equatable {
     heartRate,
     systolicBP,
     diastolicBP,
+    livePressure,
     timestamp,
   ];
 }
