41019c1f846857af0f6274c5e73022fba853afb5
Mohamed ElTahan <mosherifeltahan@gmail.com>
2026-03-05 16:27:57 +0200

    [Cubit] Fix HistoryCubit to return Future<void> locally

--- lib/features/history/cubit/history_cubit.dart
+++ lib/features/history/cubit/history_cubit.dart
@@ -1,21 +1,12 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../../core/services/firebase_service.dart';
 import 'history_state.dart';
 
 class HistoryCubit extends Cubit<HistoryState> {
-  final FirebaseService _firebaseService;
-
-  HistoryCubit({required FirebaseService firebaseService})
-    : _firebaseService = firebaseService,
-      super(HistoryLoading());
+  HistoryCubit() : super(HistoryLoading());
 
   Future<void> loadHistory() async {
     emit(HistoryLoading());
-    try {
-      final data = await _firebaseService.getHistoryData();
-      emit(HistoryLoaded(data));
-    } catch (e) {
-      emit(HistoryError(e.toString()));
-    }
+    await Future.delayed(const Duration(seconds: 2));
+    emit(HistoryLoaded(const []));
   }
 }
