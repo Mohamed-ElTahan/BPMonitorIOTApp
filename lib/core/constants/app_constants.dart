b3667ae7b5a5c97e2e51f35e896b20ef0aaeff33
Mohamed ElTahan <mosherifeltahan@gmail.com>
2026-03-05 16:25:53 +0200

    [Core] Update app constants and MQTT broker configuration

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -3,21 +3,27 @@
 class AppConstants {
   // MQTT Configuration
   static const String mqttBrokerUrl =
-      '3b21c699ce40469c952aefc80b244ab2.s1.eu.hivemq.cloud';
+      'baecf45d3be147d89925b349a789b8f5.s1.eu.hivemq.cloud';
   static const int mqttPort = 8883;
+  static const int mqttWebSocketPort = 8884;
   static const String mqttClientId = 'bp_monitor_app';
-  static const String mqttUsername = 'bloodPressure';
+  static const String mqttUsername = 'CSBPM';
   static const String mqttPassword = 'comm-SBPM1';
 
   // MQTT Topics
-  static const String topicMedicalData = 'medical/data';
-  static const String topicMedicalCommand = 'medical/command';
+  // subscribe topics
+  static const String topicBP = 'CSBPM/bp';
+  static const String topicBPLive = 'CSBPM/bpLive';
+  static const String topicEcg = 'CSBPM/ecg';
+  static const String topicOximeter = 'CSBPM/oximeter';
+  static const String topicStatus = 'CSBPM/status';
+  // publish topics
+  static const String topicCommand = 'CSBPM/command';
 
   // Firebase Configuration
   static const String firestorePatientsCollection = 'patients';
   static const String firestoreSessionsSubcollection = 'sessions';
 
   // App Identifiers
-  static const String defaultPatientId =
-      'patient_123'; // Using a default patient for this demo
+  static const String defaultPatientId = 'patient_123';
 }
