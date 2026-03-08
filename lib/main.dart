a6278c98c4a580b76b159f537689619cf85da55b
Mohamed ElTahan <mosherifeltahan@gmail.com>
2026-03-08 23:52:00 +0200

    fix(main): restore MonitorRepository and fix dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,44 @@
+import 'package:bp_monitor_iot/features/home/cubit/home_cubit.dart';
 import 'package:bp_monitor_iot/features/main/main_scaffold.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'core/theme/app_theme.dart';
-import 'core/data_source/mqtt_data_source.dart';
+import 'core/data_source/mqtt/mqtt_data_source.dart';
 import 'features/monitor/repository/monitor_repository.dart';
-import 'firebase_options.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const BPMonitorIoTApp());
+void main() {
+  runApp(const MyApp());
 }
 
-class BPMonitorIoTApp extends StatelessWidget {
-  const BPMonitorIoTApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'CSBPM',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      home: MultiRepositoryProvider(
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider(
+          create: (_) => MqttDataSource()..connect(),
+          dispose: (ds) => ds.dispose(),
+        ),
+        RepositoryProvider(
+          create: (context) =>
+              MonitorRepository(context.read<MqttDataSource>()),
+        ),
+      ],
+      child: MultiBlocProvider(
         providers: [
-          RepositoryProvider(
-            create: (_) => MqttDataSource(),
-            dispose: (ds) => ds.dispose(),
-          ),
-          RepositoryProvider(
+          BlocProvider(
             create: (context) =>
-                MonitorRepository(context.read<MqttDataSource>()),
+                HomeCubit(context.read<MqttDataSource>().client),
           ),
         ],
-        child: const MainScaffold(),
+        child: MaterialApp(
+          title: 'BP Monitor IOT',
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.lightTheme,
+          home: const MainScaffold(),
+        ),
       ),
     );
   }
