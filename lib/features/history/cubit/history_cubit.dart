7ea4bd00ec24be19df28ae75825e0d0d9ad8e255
Mohamed ElTahan <mosherifeltahan@gmail.com>
2026-03-09 22:27:12 +0200

    feat(history): update HistoryCubit to manage data via Firestore

--- lib/features/history/cubit/history_cubit.dart
+++ lib/features/history/cubit/history_cubit.dart
@@ -1,12 +1,29 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../core/data_source/firebase/firestore_service.dart';
 import 'history_state.dart';
 
 class HistoryCubit extends Cubit<HistoryState> {
-  HistoryCubit() : super(HistoryLoading());
+  final FirestoreService firestoreService;
+
+  HistoryCubit(this.firestoreService) : super(HistoryLoading());
 
   Future<void> loadHistory() async {
-    emit(HistoryLoading());
-    await Future.delayed(const Duration(seconds: 2));
-    emit(HistoryLoaded([]));
+    try {
+      emit(HistoryLoading());
+      final history = await firestoreService.getAllHistory();
+      if (history.isEmpty) {
+        emit(const HistoryLoaded([]));
+      } else {
+        emit(HistoryLoaded(history));
+      }
+    } catch (e) {
+      emit(HistoryError(e.toString()));
+    }
+  }
+
+  void listenToHistory() {
+    firestoreService.getHistoryStream().listen((history) {
+      emit(HistoryLoaded(history));
+    });
   }
 }
