b141b773ce734388fe0fdf51fabdab4f2c3963f3
Mohamed ElTahan <mosherifeltahan@gmail.com>
2026-03-09 22:28:16 +0200

    refactor(main): update entry point for new feature structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:bp_monitor_iot/features/home/cubit/home_cubit.dart';
-import 'package:bp_monitor_iot/features/main/main_scaffold.dart';
+import 'package:bp_monitor_iot/features/main_scaffold/main_scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'core/theme/app_theme.dart';
 import 'core/data_source/mqtt/mqtt_data_source.dart';
 import 'features/monitor/repository/monitor_repository.dart';
+import 'core/cubits/mqtt_connection_cubit.dart';
+import 'core/data_source/firebase/firestore_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -21,6 +27,7 @@ class MyApp extends StatelessWidget {
           create: (_) => MqttDataSource()..connect(),
           dispose: (ds) => ds.dispose(),
         ),
+        RepositoryProvider(create: (_) => FirestoreService()),
         RepositoryProvider(
           create: (context) =>
               MonitorRepository(context.read<MqttDataSource>()),
@@ -28,6 +35,10 @@ class MyApp extends StatelessWidget {
       ],
       child: MultiBlocProvider(
         providers: [
+          BlocProvider(
+            create: (context) =>
+                MqttConnectionCubit(context.read<MqttDataSource>()),
+          ),
           BlocProvider(
             create: (context) =>
                 HomeCubit(context.read<MqttDataSource>().client),
