6d13f3a029fe4f6f3de690dc0c54e1f62a1445d5
Mohamed ElTahan <mosherifeltahan@gmail.com>
2026-03-11 00:56:59 +0200

    feat: add FirestoreDataSource for history persistence

---
+++ lib/core/data_source/firebase/firestore_data_source.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:bp_monitor_iot/features/history/model/patient_model.dart';
+
+class FirestoreDataSource {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final String _collectionPath = 'history';
+
+  // Save patient measurement to Firestore
+  Future<void> saveHistory(PatientModel history) async {
+    try {
+      await _firestore.collection(_collectionPath).add(history.toJson());
+    } catch (e) {
+      throw Exception('Failed to save history: $e');
+    }
+  }
+
+  // get all patient history from firestore
+  Future<List<PatientModel>> getAllHistory() async {
+    try {
+      final snapshot = await _firestore
+          .collection(_collectionPath)
+          .orderBy('timestamp', descending: true)
+          .get();
+      return snapshot.docs.map((doc) {
+        return PatientModel.fromJson(doc.data());
+      }).toList();
+    } catch (e) {
+      throw Exception('Failed to fetch history: $e');
+    }
+  }
+}
