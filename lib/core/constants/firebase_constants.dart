081274c734f0a977de3ddfb30bb432b7e1182853
Mohamed ElTahan <mosherifeltahan@gmail.com>
2026-03-12 14:54:29 +0200

    firebase constants

---
+++ lib/core/constants/firebase_constants.dart
@@ -0,0 +1,25 @@
+class FirebaseConstants {
+  // Collections
+  static const String collectionHistory = 'history';
+
+  // Keys
+  static const String keyName = 'name';
+  static const String keyGender = 'gender';
+  static const String keyAge = 'age';
+  static const String keyBloodPressure = 'bloodPressure';
+  static const String keyOximeter = 'oximeter';
+  static const String keyLivePressure = 'livePressure';
+  static const String keyEcg = 'ecg';
+  static const String keyTimestamp = 'timestamp';
+
+  // BP Keys
+  static const String keySystolic = 'systolic';
+  static const String keyDiastolic = 'diastolic';
+
+  // Oximeter Keys
+  static const String keySpo2 = 'spo2';
+  static const String keyHr = 'hr';
+
+  // Backward compatibility keys
+  static const String keyHeartRate = 'heartRate';
+}
