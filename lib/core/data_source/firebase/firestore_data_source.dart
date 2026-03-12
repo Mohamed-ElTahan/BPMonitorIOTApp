f46bf500e0e7c1ebe21423f7177be6e4012f182d
Mohamed ElTahan <mosherifeltahan@gmail.com>
2026-03-12 14:54:45 +0200

    firbase data source

--- lib/core/data_source/firebase/firestore_data_source.dart
+++ lib/core/data_source/firebase/firestore_data_source.dart
@@ -1,9 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:bp_monitor_iot/features/history/model/patient_model.dart';
+import '../../constants/firebase_constants.dart';
 
 class FirestoreDataSource {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final String _collectionPath = 'history';
+  final String _collectionPath = FirebaseConstants.collectionHistory;
 
   // Save patient measurement to Firestore
   Future<void> saveHistory(PatientModel history) async {
@@ -19,13 +20,22 @@ class FirestoreDataSource {
     try {
       final snapshot = await _firestore
           .collection(_collectionPath)
-          .orderBy('timestamp', descending: true)
+          .orderBy(FirebaseConstants.keyTimestamp, descending: true)
           .get();
       return snapshot.docs.map((doc) {
-        return PatientModel.fromJson(doc.data());
+        return PatientModel.fromJson(doc.data(), id: doc.id);
       }).toList();
     } catch (e) {
       throw Exception('Failed to fetch history: $e');
+    }
+  }
+
+  // delete patient measurement record from Firestore
+  Future<void> deletePatientRecord(String id) async {
+    try {
+      await _firestore.collection(_collectionPath).doc(id).delete();
+    } catch (e) {
+      throw Exception('Failed to delete patient record: $e');
     }
   }
 }
