b64c8cb872ab97e5d7bb6448eab1bd5730d6da44
Mohamed ElTahan <mosherifeltahan@gmail.com>
2026-03-14 01:17:24 +0200

    Extract team info to dedicated directory

---
+++ lib/features/about/team/model/list_team_member.dart
@@ -0,0 +1,60 @@
+import 'member_model.dart';
+
+class ListTeamMember {
+  static final List<MemberModel> teamData = const [
+    MemberModel(
+      name: 'Mohamed Sherif Abbass',
+      role: 'Flutter Developer',
+      email: 'mosherieltahan@gmail.com',
+      imagePath: 'assets/team_image/mohamed.png',
+    ),
+    MemberModel(
+      name: 'Test',
+      role: 'Test',
+      email: 'Test@gmail.com',
+      imagePath: 'assets/team_image/profile.png',
+    ),
+    MemberModel(
+      name: 'Test',
+      role: 'Test',
+      email: 'Test@gmail.com',
+      imagePath: 'assets/team_image/profile.png',
+    ),
+    MemberModel(
+      name: 'Test',
+      role: 'Test',
+      email: 'Test@gmail.com',
+      imagePath: 'assets/team_image/profile.png',
+    ),
+    MemberModel(
+      name: 'Test',
+      role: 'Test',
+      email: 'Test@gmail.com',
+      imagePath: 'assets/team_image/profile.png',
+    ),
+    MemberModel(
+      name: 'Test',
+      role: 'Test',
+      email: 'Test@gmail.com',
+      imagePath: 'assets/team_image/profile.png',
+    ),
+    MemberModel(
+      name: 'Test',
+      role: 'Test',
+      email: 'Test@gmail.com',
+      imagePath: 'assets/team_image/profile.png',
+    ),
+    MemberModel(
+      name: 'Test',
+      role: 'Test',
+      email: 'Test@gmail.com',
+      imagePath: 'assets/team_image/profile.png',
+    ),
+    MemberModel(
+      name: 'Test',
+      role: 'Test',
+      email: 'Test@gmail.com',
+      imagePath: 'assets/team_image/profile.png',
+    ),
+  ];
+}
---
+++ lib/features/about/team/team_info_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import '../../../core/constants/app_strings.dart';
+import 'model/list_team_member.dart';
+import 'widgets/member_card.dart';
+
+class TeamInfoScreen extends StatelessWidget {
+  const TeamInfoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text(AppStrings.teamInfo), centerTitle: true),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          final double itemWidth = (constraints.maxWidth - 20 - 10) / 2;
+          final double itemHeight = (constraints.maxHeight - 20 - 10) / 2.5;
+          final double childAspectRatio = itemWidth / itemHeight;
+
+          return GridView.builder(
+            padding: const EdgeInsets.all(10),
+            itemCount: ListTeamMember.teamData.length,
+            physics: const ScrollPhysics(),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: childAspectRatio,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+            ),
+            itemBuilder: (context, index) =>
+                MemberCard(member: ListTeamMember.teamData[index]),
+          );
+        },
+      ),
+    );
+  }
+}
