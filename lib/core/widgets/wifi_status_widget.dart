4635f33b55f112a45a3e045ddf87e65e4fc4fe7a
Mohamed ElTahan <mosherifeltahan@gmail.com>
2026-03-13 14:47:19 +0200

    improve wifi widget

--- lib/core/widgets/wifi_status_widget.dart
+++ lib/core/widgets/wifi_status_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../cubits/wifi_cubit.dart';
-import '../theme/app_colors.dart';
+import 'connection_status_badge.dart';
 
 class WifiStatusWidget extends StatelessWidget {
   const WifiStatusWidget({super.key});
@@ -10,104 +10,33 @@ class WifiStatusWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<WifiCubit, WifiStatus>(
       builder: (context, status) {
-        final (color, icon, label) = switch (status) {
-          WifiStatus.wifi => (AppColors.ecgGreen, Icons.wifi_rounded, 'WiFi'),
+        final (connectionStatus, icon, label) = switch (status) {
+          WifiStatus.wifi => (
+            ConnectionStatus.connected,
+            Icons.wifi_rounded,
+            'WiFi'
+          ),
           WifiStatus.mobile => (
-            AppColors.bpAmber,
+            ConnectionStatus.connecting,
             Icons.signal_cellular_alt_rounded,
-            'Mobile',
+            'Mobile'
           ),
           WifiStatus.none => (
-            AppColors.heartRateRed,
+            ConnectionStatus.disconnected,
             Icons.wifi_off_rounded,
-            'No Network',
+            'No Network'
           ),
         };
 
-        return Container(
-          margin: const EdgeInsets.symmetric(vertical: 8),
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
-          decoration: BoxDecoration(
-            color: color.withValues(alpha: 0.1),
-            borderRadius: BorderRadius.circular(20),
-            border: Border.all(color: color.withValues(alpha: 0.2)),
-          ),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              _PulsingDot(color: color),
-              const SizedBox(width: 5),
-              Icon(icon, size: 11, color: color.withValues(alpha: 0.9)),
-              const SizedBox(width: 4),
-              Text(
-                label,
-                style: TextStyle(
-                  color: color.withValues(alpha: 0.9),
-                  fontSize: 10,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 0.5,
-                ),
-              ),
-            ],
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          child: ConnectionStatusBadge(
+            status: connectionStatus,
+            label: label,
+            icon: icon,
           ),
         );
       },
-    );
-  }
-}
-
-/// Animated pulsing dot that breathes when connected.
-class _PulsingDot extends StatefulWidget {
-  final Color color;
-  const _PulsingDot({required this.color});
-
-  @override
-  State<_PulsingDot> createState() => _PulsingDotState();
-}
-
-class _PulsingDotState extends State<_PulsingDot>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 1200),
-    )..repeat(reverse: true);
-    _animation = Tween<double>(
-      begin: 0.4,
-      end: 1.0,
-    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _animation,
-      builder: (_, _) => Container(
-        width: 6,
-        height: 6,
-        decoration: BoxDecoration(
-          color: widget.color.withValues(alpha: _animation.value),
-          shape: BoxShape.circle,
-          boxShadow: [
-            BoxShadow(
-              color: widget.color.withValues(alpha: _animation.value * 0.5),
-              blurRadius: 4,
-              spreadRadius: 1,
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
