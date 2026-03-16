ec132552a513d31db267921e3a7b36eed91a1f00
Mohamed ElTahan <mosherifeltahan@gmail.com>
2026-03-16 21:58:03 +0200

    feat: add analysis data model

---
+++ lib/features/analysis/model/analysis_model.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+enum VitalsStatus {
+  normal,
+  warning,
+  critical,
+}
+
+class VitalInterpretation {
+  final String label;
+  final VitalsStatus status;
+  final Color color;
+  final String description;
+
+  const VitalInterpretation({
+    required this.label,
+    required this.status,
+    required this.color,
+    required this.description,
+  });
+}
+
+class AnalysisResult {
+  final VitalInterpretation bp;
+  final VitalInterpretation hr;
+  final VitalInterpretation spo2;
+
+  const AnalysisResult({
+    required this.bp,
+    required this.hr,
+    required this.spo2,
+  });
+}
