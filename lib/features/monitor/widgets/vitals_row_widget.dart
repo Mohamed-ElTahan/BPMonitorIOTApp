69865ab0f608a537167c3ad21a24cdf4c36b9673
Mohamed ElTahan <mosherifeltahan@gmail.com>
2026-04-06 21:04:56 +0200

    refactor(monitor): show triple vitals inkl. estimated BP and responsive layout

--- lib/features/monitor/widgets/vitals_row_widget.dart
+++ lib/features/monitor/widgets/vitals_row_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../../core/constants/app_strings.dart';
 import '../../../core/theme/app_colors.dart';
+import '../cubit/bp_estimator.dart';
 import 'vitals_card.dart';
 
 class VitalsRowWidget extends StatelessWidget {
@@ -21,12 +22,18 @@ class VitalsRowWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double halfWidth = (MediaQuery.of(context).size.width - 40) / 2;
+    // For 3 elements in a row with spacing 8 and padding 16: 16 + 8 + 8 = 32.
+    // Let's use 40 just to be safe so it doesn't wrap unexpectedly.
+    final double thirdWidth = (MediaQuery.of(context).size.width - 40) / 3;
+    final estimatedBp = BpEstimator.estimate(hr, spo2);
+
     return Wrap(
       spacing: 8,
       runSpacing: 8,
       children: [
         SizedBox(
-          width: (MediaQuery.of(context).size.width - 44) / 2,
+          width: halfWidth,
           child: VitalsCard(
             title: AppStrings.livePressure,
             value: livePressure.isNotEmpty
@@ -38,7 +45,7 @@ class VitalsRowWidget extends StatelessWidget {
           ),
         ),
         SizedBox(
-          width: (MediaQuery.of(context).size.width - 44) / 2,
+          width: halfWidth,
           child: VitalsCard(
             title: AppStrings.bloodPressure,
             value: "${sys.toStringAsFixed(1)}/${dia.toStringAsFixed(1)}",
@@ -48,25 +55,37 @@ class VitalsRowWidget extends StatelessWidget {
           ),
         ),
         SizedBox(
-          width: (MediaQuery.of(context).size.width - 44) / 2,
+          width: thirdWidth,
           child: VitalsCard(
             title: AppStrings.heartRate,
-            value: "${hr.toInt()} ${AppStrings.unitBpm}",
+            value: hr.toInt().toString(),
+            subtitle: AppStrings.unitBpm,
             color: AppColors.heartRateRed,
-            icon: Icons.favorite,
+            icon: Icons.heart_broken_outlined,
           ),
         ),
         SizedBox(
-          width: (MediaQuery.of(context).size.width - 44) / 2,
+          width: thirdWidth,
           child: VitalsCard(
-            title: AppStrings.spo2,
-            value: "$spo2${AppStrings.unitPercentage}",
+            title: AppStrings.oxygen,
+            value: "${spo2.toString()}%",
+            subtitle: AppStrings.spo2,
             color: AppColors.spo2Cyan,
             icon: Icons.water_drop,
+          ),
+        ),
+        SizedBox(
+          width: thirdWidth,
+          child: VitalsCard(
+            title: AppStrings.estimatedBp,
+            value:
+                "${estimatedBp.systolic.toStringAsFixed(0)}/${estimatedBp.diastolic.toStringAsFixed(0)}",
+            subtitle: AppStrings.unitMmHg,
+            color: Colors.deepPurpleAccent,
+            icon: Icons.analytics,
           ),
         ),
       ],
     );
   }
 }
-
